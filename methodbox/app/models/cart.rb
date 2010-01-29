2622a4349a72e56bcf0dd327421ad27d7d8305b9
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-01-29 15:55:27 +0000

    remove variables from cart

--- methodbox/app/models/cart.rb
+++ methodbox/app/models/cart.rb
@@ -10,4 +10,14 @@ class Cart
       @items << variable
     end
   end
+
+  def remove_variable(variable)
+    puts "remove variable " + variable
+    @items.delete_if{|ci| ci.to_s == variable.to_s}
+    #ci.to_s == variable.to_s
+#    if (@items.index(variable)!=nil)
+#      puts "removing " + variable
+#      @items.delete(variable)
+#    end
+  end
 end
