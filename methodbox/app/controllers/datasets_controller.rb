a5fd926378c545f67bf070383e1ee435c7abeeee
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-02-22 16:01:29 +0000

    show and index for datasets

--- methodbox/app/controllers/datasets_controller.rb
+++ methodbox/app/controllers/datasets_controller.rb
@@ -1,3 +1,25 @@
 class DatasetsController < ApplicationController
+
+  before_filter :login_required
+  before_filter :find_datasets, :only => [ :index ]
+  before_filter :find_dataset, :only => [ :show ]
+
+  def index
+
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def find_dataset
+    @dataset = Dataset.find(params[:id])
+  end
+
+  def find_datasets
+    @datasets = Dataset.find(params[:all])
+  end
   
 end
