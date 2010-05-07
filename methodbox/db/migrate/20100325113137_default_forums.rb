2f08f7f2cb1868d6b2a3c3b08dc57ff9ed9c9043
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-07 15:01:30 +0000

    Deleted migration which adds key_variable (see previous update) Commented default migration with dependency rake

--- methodbox/db/migrate/20100325113137_default_forums.rb
+++ methodbox/db/migrate/20100325113137_default_forums.rb
@@ -1,5 +1,8 @@
 require 'default_data_migration'
 
+#Use note: You must set up savage beast forums first
+#rake savage_beast:bootstrap_db
+
 class DefaultForums < DefaultDataMigration
   def self.model_class_name
     "Forum"
--- methodbox/db/migrate/20100331152611_add_key_variable_to_dataset.rb
+++
@@ -1,9 +0,0 @@
-class AddKeyVariableToDataset < ActiveRecord::Migration
-  def self.up
-    add_column :datasets, :key_variable, :string
-  end
-
-  def self.down
-    remove_column :datasets, :key_variable
-  end
-end
