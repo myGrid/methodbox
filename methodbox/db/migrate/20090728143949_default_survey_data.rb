ac2a697f69987a5e0385e9d73523daf66c3ec53a
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-05-10 13:00:51 +0000

    remove default data migrations, do as rake task instead

--- methodbox/db/migrate/20090728143949_default_survey_data.rb
+++
@@ -1,9 +0,0 @@
-require 'default_data_migration'
-
-
-class DefaultSurveyData < DefaultDataMigration
-  def self.model_class_name
-    "Survey"
-#    Survey.rebuild_solr_index
-  end
-end
--- methodbox/db/migrate/20100205152826_default_dataset_data.rb
+++
@@ -1,8 +0,0 @@
-require 'default_data_migration'
-
-
-class DefaultDatasetData < DefaultDataMigration
-  def self.model_class_name
-    "Dataset"
-  end
-end
