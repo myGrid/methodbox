907a0a31a83f8f0b7d79dbf1d0b5af3e885a9099
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-14 12:09:22 +0000

    Revert of r574 removed Christian Folder

--- Christian/environment_local.rb
+++
@@ -1,9 +0,0 @@
-EMAIL_ENABLED = true
-CSV_SERVER_LOCATION='localhost'
-CSV_SERVER_PORT='25000'
-CSV_SERVER_PATH='/csv-server/eos'
-CSV_FILE_PATH='/elabs/csv/'
-REGISTRATION_CLOSED = false
-NEW_CSV_FILE_PATH='/elabs/csv/complete'
-METADATA_PATH='/Users/Ian/obesity_data/metadata/'
-UKDA_EMAIL_ADDRESS='http://oai.esds.ac.uk:8080/NesstarLogin/login.do'
