70b9b1519254ab1f5fa02d0f7cf3a9c857a7b3b1
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-19 16:01:31 +0000

    Renamed tests

--- methodbox/test/functional/scripts_controller_test.rb
+++ methodbox/test/functional/scripts_controller_test.rb
@@ -5,27 +5,27 @@ class ScriptsControllerTest < ActionController::TestCase
 
   include AuthenticatedTestHelper
 
-  test "visible to logged in" do
+  test "scripts visible to logged in" do
     login_as(:aaron)
     get :index
     assert_response :success
     assert_nil flash[:error]
   end
 
-  test "invisible to not logged in" do
+  test "scripts invisible to not logged in" do
     get :index
     assert_response :redirect
     assert_redirected_to :controller => "session", :action => "new"
     assert_not_nil flash[:error]
   end
 
-  test "help visible to not logged in" do
+  test "scripts/help visible to not logged in" do
     get :help
     assert_response :success
     assert_nil flash[:error]
   end
 
-  test "help2 visible to not logged in" do
+  test "scripts/help2 visible to not logged in" do
     get :help2
     assert_response :success
     assert_nil flash[:error]
