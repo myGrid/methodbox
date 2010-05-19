ac8aa9f5230ddcf24a664c2333a652769ab989ce
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-19 14:38:29 +0000

    Added testing for (593) granting admin access.

--- methodbox/test/functional/admin_controller_test.rb
+++ methodbox/test/functional/admin_controller_test.rb
@@ -17,9 +17,34 @@ class AdminControllerTest < ActionController::TestCase
     login_as(:aaron)
     get :show
     assert_response :redirect
+    assert_redirected_to :controller => "home", :action => "index"
     assert_not_nil flash[:error]
   end
 
+  test "Illegal call to make self admin" do
+    login_as(:aaron)
+    aaron = users(:aaron)
+    post :change_admin_status, :id => aaron.id, :is_admin => true
+    assert_not_nil flash[:error]
+    assert_redirected_to :controller => "home", :action => "index"
+  end
   
+  test "Legal call to make user admin" do
+    login_as(:quentin)
+    aaron = users(:aaron)
+    post :change_admin_status, :id => aaron.id, :is_admin => true
+    assert_nil flash[:error]
+    assert_not_nil flash[:notice]
+    assert_redirected_to :controller => "admin", :action => "index"
+  end
+
+  test "Illegal call to revoke own admin" do
+    login_as(:quentin)
+    quentin = users(:quentin)
+    post :change_admin_status, :id => quentin.id, :is_admin => false
+    quentin = users(:quentin)
+    assert_not_nil flash[:error]
+    assert_redirected_to :controller => "home", :action => "index"
+  end
 
 end
