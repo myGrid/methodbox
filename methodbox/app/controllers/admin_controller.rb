f5cb5bf924626f2783e31fa0536be2a40a0acb3b
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-19 09:34:17 +0000

    Updated admin page to allow the granting and revoking of admin rights.

--- methodbox/app/controllers/admin_controller.rb
+++ methodbox/app/controllers/admin_controller.rb
@@ -4,7 +4,33 @@ class AdminController < ApplicationController
 
   def show
     
-    
+  end
+  
+  def change_admin_status
+    @user = User.find(params[:id])
+    is_admin = params[:is_admin]
+    if is_admin
+      @user.is_admin = true
+      @user.save
+      if (current_user == @user)
+        flash[:notice]="You are now an administrator."
+      else
+        flash[:notice]= @user.id.to_s + " is now an administrator."
+      end
+    else  
+      if (User.count :conditions => "is_admin = true") == 1
+        error("You can not revoke the last admin.", "is the last admin. Rovoke Rejected.")
+        return
+      end
+      if (current_user == @user)
+        error("As a safety feature you can not revoke your own admin rights. Ask another admin to do it for you.", "Attempt to revoke own admin rights blocked.")
+        return
+      end  
+      @user.is_admin = false
+      @user.save
+      flash[:notice]= @user.id.to_s + " is no longer an administrator."
+    end	      
+    redirect_to :action => "index"
   end
   
 end
