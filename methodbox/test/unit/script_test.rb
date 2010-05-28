70c1fdee60bcfde32e9127a4a4b28fcbdbcb507f
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-28 21:22:38 +0000

    updated script_test

--- methodbox/test/unit/script_test.rb
+++ methodbox/test/unit/script_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class ScriptTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_create_new_ok
+    assert_difference 'Script.count' do
+      s = create_script()
+    end
   end
+
+  def test_no_title_fails
+    assert_no_difference 'Script.count' do
+      s = create_script(:title => nil)
+    end
+  end
+
+  def test_no_filename_fails
+    assert_no_difference 'Script.count' do
+      s = create_script(:original_filename => nil)
+    end
+  end
+
+protected
+  def create_script(options = {})
+    record = Script.new({ :title => 'From Script Test', :body => "Body for the test", :description => "this is where I would decribe it", :content_type => "Some context type", :original_filename => "data.gif"}.merge(options))
+    record.save
+    record
+  end
+
 end
+
