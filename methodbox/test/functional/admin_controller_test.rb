8923f6a6c24a7e3ff4ae6611cfe24425be99a29c
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-28 13:08:49 +0000

    admin_controller_test.rb cleaned up

--- methodbox/test/functional/admin_controller_test.rb
+++ methodbox/test/functional/admin_controller_test.rb
@@ -7,22 +7,22 @@ class AdminControllerTest < ActionController::TestCase
     get :show
     assert_response :success
     assert_nil flash[:error]
+    assert_nil flash[:notice]
   end
 
   test "invisible to non admin" do
     login_as(:aaron)
     get :show
+    assert_equal "Admin rights required", flash[:error]
     assert_response :redirect
-    assert_redirected_to :controller => "home", :action => "index"
-    assert_not_nil flash[:error]
   end
 
   test "Illegal call to make self admin" do
     login_as(:aaron)
     aaron = users(:aaron)
     post :change_admin_status, :id => aaron.id, :is_admin => true
-    assert_not_nil flash[:error]
-    assert_redirected_to :controller => "home", :action => "index"
+    assert_equal "Admin rights required", flash[:error]
+    assert_response :redirect
   end
   
   test "Legal call to make user admin" do
@@ -30,8 +30,8 @@ class AdminControllerTest < ActionController::TestCase
     aaron = users(:aaron)
     post :change_admin_status, :id => aaron.id, :is_admin => true
     assert_nil flash[:error]
-    assert_not_nil flash[:notice]
-    assert_redirected_to :controller => "admin", :action => "index"
+    assert_match /is now an administrator./, flash[:notice]
+    assert_response :redirect
   end
 
   test "Illegal call to revoke own admin" do
@@ -40,7 +40,7 @@ class AdminControllerTest < ActionController::TestCase
     post :change_admin_status, :id => quentin.id, :is_admin => false
     quentin = users(:quentin)
     assert_not_nil flash[:error]
-    assert_redirected_to :controller => "home", :action => "index"
+    assert_response :redirect
   end
 
 end
