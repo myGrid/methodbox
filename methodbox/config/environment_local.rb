a3bc529ea52abc29b0b375ee1593215e8bd9c69e
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-05-28 11:13:07 +0000

    SANDBOX flag

--- methodbox/config/environment_local.rb
+++ methodbox/config/environment_local.rb
@@ -1,9 +1,10 @@
+SANDBOX = true
 EMAIL_ENABLED = true
 CSV_SERVER_LOCATION='localhost'
 CSV_SERVER_PORT='25000'
 CSV_SERVER_PATH='/csv-server/eos'
 CSV_FILE_PATH='/Users/Ian/obesity_data/csv/'
-REGISTRATION_CLOSED = true
+REGISTRATION_CLOSED = false
 NEW_CSV_FILE_PATH='/Users/Ian/obesity_data/csv/complete'
 METADATA_PATH='/Users/Ian/obesity_data/metadata/'
 UKDA_EMAIL_ADDRESS='http://oai.esds.ac.uk:8080/NesstarLogin/login.do'
