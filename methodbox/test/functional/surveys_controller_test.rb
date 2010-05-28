29c7e17a24609e5ef1c62dce4efddc76a32f414e
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-05-28 13:12:09 +0000

    moved access tests out of surveys_controller_test.rb

--- methodbox/test/functional/surveys_controller_test.rb
+++ methodbox/test/functional/surveys_controller_test.rb
@@ -2,24 +2,5 @@ require 'test_helper'
 
 class SurveysControllerTest < ActionController::TestCase
 
-  test "visible to logged in" do
-    login_as(:aaron)
-    get :index
-    assert_response :success
-    assert_nil flash[:error]
-  end
-
-  test "invisible to not logged in" do
-    get :index
-    assert_response :redirect
-    assert_redirected_to :controller => "session", :action => "new"
-    assert_not_nil flash[:error]
-  end
-
-  test "help visible to not logged in" do
-    get :help
-    assert_response :success
-    assert_nil flash[:error]
-  end
 
 end
