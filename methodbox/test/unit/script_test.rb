9f9432d6fffac62df2cc1da1297829f1227a3a5c
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-06-03 10:03:34 +0000

    Proposed Unit Test for scripts. Need to decide how strict to be.

--- methodbox/test/unit/script_test.rb
+++ methodbox/test/unit/script_test.rb
@@ -14,18 +14,147 @@ class ScriptTest < ActiveSupport::TestCase
     end
   end
 
-  def test_no_filename_fails
+  def test_empty_title_fails
+    assert_no_difference 'Script.count' do
+      s = create_script(:title => "")
+    end
+  end
+
+  def test_body_unused
+    assert_difference 'Script.count' do
+      s = create_script(:body => nil)
+    end
+  end
+
+#Too Strick?
+#  def test_no_description_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:description => nil)
+#    end
+#  end
+
+  def test_empty_description_ok
+    assert_difference 'Script.count' do
+      s = create_script(:description => "")
+    end
+  end
+
+#Too Strick?
+#  def test_no_content_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:content_type => nil)
+#    end
+#  end
+
+#Too Strick?
+#  def test_empty_content_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:content_type => "")
+#    end
+#  end
+
+#Too Strick?
+#  def test_no_contributor_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:contributor_type => nil)
+#    end
+#  end
+
+#Too Strick?
+#  def test_empty_contributor_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:contributor_type => "")
+#    end
+#  end
+
+#Too Strick?
+#  def test_no_contributor_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:contributor_id => nil)
+#    end
+#  end
+
+#Too Strick?
+#  def test_bad_contributor_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:contributor_id => -1)
+#    end
+#  end
+
+#Too Strick?
+#  def test_no_content_blob_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script()
+#      s.save
+#    end
+#  end
+
+#Too Strick?
+# def test_bad_content_blob_fails #Test fails June 3 2010
+#    assert_no_difference 'Script.count' do
+#      s = create_script()
+#      s.content_blob_id = -1
+#      s.save
+#    end
+#  end
+
+  def test_no_original_filename_fails
     assert_no_difference 'Script.count' do
       s = create_script(:original_filename => nil)
     end
   end
 
-protected
-  def create_script(options = {})
-    record = Script.new({ :title => 'From Script Test', :body => "Body for the test", :description => "this is where I would decribe it", :content_type => "Some context type", :original_filename => "data.gif"}.merge(options))
-    record.save
-    record
+  def test_empty_original_filename_ok
+    assert_no_difference 'Script.count' do
+      s = create_script(:original_filename => "")
+    end
   end
 
+#Too Strick?
+#  def test_no_method_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:method_type => nil)
+#    end
+#  end
+
+#Too Strick?
+#  def test_empty_method_type_fails
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:method_type => "")
+#    end
+#  end
+
+#Too Strick?
+#  def test_bad_method_type_fails #Test fails June 3 2010
+#    assert_no_difference 'Script.count' do
+#      s = create_script(:method_type => "bad")
+#    end
+#  end
+
+protected
+  def test_script(options = {})
+    Script.new({ :title => 'From Script Test', :body => "Body for the test", :description => "this is where I would decribe it", :content_type => "Some context type", :contributor_type => "User", :contributor_id => users(:aaron).id, :original_filename => "data.gif", :method_type => "Other"}.merge(options))
+  end
+
+  def create_script(options = {})
+    script = test_script(options)
+    script.content_blob = content_blobs(:content_blob_with_little_file)
+    script.save
+    script
+  end
 end
 
+#  create_table "scripts", :force => true do |t|
+#    t.string   "title"
+#    t.text     "body"
+#    t.string   "description"
+#    t.datetime "created_at"
+#    t.datetime "updated_at"
+#    t.string   "content_type"
+#    t.string   "contributor_type"
+#    t.integer  "contributor_id"
+#    t.integer  "content_blob_id"
+#    t.datetime "last_used_at"
+#    t.string   "original_filename"
+#    t.string   "method_type"
+#  end
