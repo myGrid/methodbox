e871fcbe5c940751134b81b45296e61212453e81
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-06-03 06:43:00 +0000

    Test for uppdate r755

--- methodbox/test/functional/scripts_controller_test.rb
+++ methodbox/test/functional/scripts_controller_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class ScriptsControllerTest < ActionController::TestCase
 
-#new empty file and title = "sdfasdfa sdfsdfasdfa" gives an error
+  #No test for flash[:error] as popped up rather than set
+
+  def test_empty_file
+    login_as :aaron
+    assert_no_difference 'Script.count' do
+	    post :create, :script => {:title => 'test script', :data => ''}
+	 end
+  end
 
 end
