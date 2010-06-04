c959e50c968dc772d2fa4d277d3558df0a6da234
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-06-04 09:31:50 +0000

    A few more script controller tests

--- methodbox/test/functional/scripts_controller_test.rb
+++ methodbox/test/functional/scripts_controller_test.rb
@@ -4,28 +4,39 @@ class ScriptsControllerTest < ActionController::TestCase
 
   #No test for flash[:error] as popped up rather than set
 
-  def test_create_no_script
+  def test_create_no_script_refused
     login_as :aaron
     assert_no_difference 'Script.count' do
       post :create, :title => 'test script', :data => ''
     end
+    assert_select "h1", "New Script"
   end
 
-  def test_empty_file
-    login_as :aaron
-    assert_no_difference 'Script.count' do
-      post :create, :script => {:title => 'test script', :data => ''}
-    end
+  def test_no_data_refused
+    create_script_refused(:data => nil)
+  end
+
+  def test_empty_data_refused
+    file = MethodBoxTempFile.new('', 'text','fake.txt')
+    create_script_refused(:data => file)
+  end
+
+  def test_no_content_type_ok
+    file = MethodBoxTempFile.new('The Data', nil ,'fake.txt')
+    create_script_ok(:data => file)
+  end
+
+  def test_no_original_filename_refused
+    file = MethodBoxTempFile.new('The Data', 'text', nil)
+    create_script_refused(:data => file)
+  end
+
+  def test_no_description_ok
+    create_script_ok(:description => nil)
   end
 
   def test_create_ok
-    login_as :aaron
-    assert_difference 'Script.count' do
-      create_script()
-    end
-    assert_nil flash[:error]
-    assert_nil flash[:notice]
-    assert_response :redirect
+    create_script_ok()
    end
 
   #FIXME There must be a better way of doing this but Tempfile didn't have the size method required.
@@ -62,9 +73,29 @@ class ScriptsControllerTest < ActionController::TestCase
 protected
   def create_script(options = {})
     temp_file = MethodBoxTempFile.new('The Data','text','fake.txt')
-    post :create, :script => {:title => 'From Script Test', :body => "Body for the test", :description => "this is where I would decribe it", :contributor_type => "User", :data => temp_file}, :method_type => "Other"
+    post :create, :script => {:title => 'FromScriptTest', :body => "Body for the test", :description => "this is where I would decribe it", :contributor_type => "User", :data => temp_file}.merge(options), :method_type => "Other"
   end
 
+  def create_script_ok(options = {})
+    login_as :aaron
+    assert_difference 'Script.count' do
+      create_script(options)
+      s = Script.find_by_title ('FromScriptTest')
+      assert_nil flash[:error]
+      assert_nil flash[:notice]
+      assert_response :redirect
+      action =  s.id.to_s + "-" + s.title.to_s.downcase
+      assert_redirected_to :controller => "scripts", :action => action
+    end
+  end
+
+  def create_script_refused(options = {})
+    login_as :aaron
+    assert_no_difference 'Script.count' do
+      create_script(options)
+      assert_select "h1", "New Script"
+    end
+  end
 
 #"script"=> {
 #  "title"=>"Test 1",
