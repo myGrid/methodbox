f23b05a6bda45085b49e03b4eeef7e3635855765
Christian Brenninkmeijer <Christian@mygrid.org.uk>
2010-06-07 14:19:09 +0000

    More tidy up of people fixture and tests

--- methodbox/test/unit/person_test.rb
+++ methodbox/test/unit/person_test.rb
@@ -2,23 +2,25 @@ require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
   
-  def xest_registered
-    registered=Person.registered
-    registered.each do |p|
-      assert_not_nil p.user
-    end
-    assert registered.include?(people(:one))
-    assert !registered.include?(people(:person_without_user))
-  end
+  #Poeple without name not supported
+  #def test_registered
+  #  registered=Person.registered
+  #  registered.each do |p|
+  #    assert_not_nil p.user
+  #  end
+  #  assert registered.include?(people(:normal_person))
+  #  assert !registered.include?(people(:unactivated_person))
+  #end
 
-  def xest_without_group
-    without_group=Person.without_group
-    without_group.each do |p|
-      assert_equal 0,p.group_memberships.size
-    end
-    assert !without_group.include?(people(:one))
-    assert without_group.include?(people(:person_without_group))
-  end
+  #to look at when doing groups
+  #def test_without_group
+  #  without_group=Person.without_group
+  #  without_group.each do |p|
+  #    assert_equal 0,p.group_memberships.size
+  #  end
+  #  assert !without_group.include?(people(:one))
+  #  assert without_group.include?(people(:person_without_group))
+  #end
 
   #June 8 to do with new fixtures
   #def xest_expertise
@@ -36,59 +38,59 @@ class PersonTest < ActiveSupport::TestCase
   #  assert_equal 2,p.projects.size
   #end
   
-  def xest_userless_people
-    peeps=Person.userless_people
-    assert_not_nil peeps
-    assert peeps.size>0,"There should be some userless people"
-    assert_nil peeps.find{|p| !p.user.nil?},"There should be no people with a non nil user"    
+  #June 8 No longer doing userless people
+  #def xest_userless_people
+  #  peeps=Person.userless_people
+  #  assert_not_nil peeps
+  #  assert peeps.size>0,"There should be some userless people"
+  # assert_nil peeps.find{|p| !p.user.nil?},"There should be no people with a non nil user"    
 
-    p=people(:three)
-    assert_not_nil peeps.find{|person| p.id==person.id},"Person :three should be userless and therefore in the list"
+  #  p=people(:three)
+  #  assert_not_nil peeps.find{|person| p.id==person.id},"Person :three should be userless and therefore in the list"
 
-    p=people(:one)
-    assert_nil peeps.find{|person| p.id==person.id},"Person :one should have a user and not be in the list"
-  end
+  #  p=people(:one)
+  #  assert_nil peeps.find{|person| p.id==person.id},"Person :one should have a user and not be in the list"
+  #end
 
-  def xest_name
+  def test_name
       p=people(:normal_person)
       assert_equal "Aaron Spiggle", p.name
       p.first_name="Tom"
       assert_equal "Tom Spiggle", p.name
   end
 
-  def xest_email_with_name
+  def test_email_with_name
     p=people(:normal_person)
     assert_equal("Aaron Spiggle <aaron@example.com>",p.email_with_name)
   end
   
-  def xest_capitalization_with_nil_last_name
-    p=people(:no_first_name)
-    assert_equal " Lastname",p.name
+  def test_capitalization_with_no_first_name
+    p=create_person(:first_name => nil)
+    assert_equal " Tomson",p.name
   end
 
-  def xest_capitalization_with_nil_first_name
-    p=people(:no_last_name)
-    assert_equal "Firstname ",p.name
+  def test_capitalization_with_no_last_name
+    p=create_person(:last_name => nil)
+    assert_equal "Micheal ",p.name
   end
 
-  def xest_double_firstname_capitalised
-    p=people(:double_firstname)
-    assert_equal "Fred David Bloggs", p.name
+  def test_double_firstname_capitalised
+    p=create_person(:first_name => "fred david")
+    assert_equal "Fred David Tomson", p.name
   end
 
-  def xest_double_lastname_capitalised
-    p=people(:double_lastname)
-    assert_equal "Fred Smith Jones",p.name
+  def test_double_lastname_capitalised
+    p=create_person(:last_name => "smith jones")
+    assert_equal "Micheal Smith Jones",p.name
   end
 
-  def xest_double_barrelled_lastname_capitalised
-    p=people(:double_barrelled_lastname)
-    assert_equal "Fred Smith-Jones",p.name
+  def test_double_barrelled_lastname_capitalised
+    p=create_person(:last_name => "smith-jones")
+    assert_equal "Micheal Smith-Jones",p.name
   end
 
-  #May 17, 2010 this test currently failed. I have not looked into why yet. Christian  
-  def xest_valid
-    p=people(:one)
+  def test_valid
+    p=people(:normal_person)
     assert p.valid?
     p.email=nil
     assert !p.valid?
@@ -130,12 +132,19 @@ class PersonTest < ActiveSupport::TestCase
     assert p.valid?
   end
 
-  def xest_email_unique
-    p=people(:one)
+  def test_email_unique
+    p=people(:normal_person)
     newP=Person.new(:first_name=>"Fred",:email=>p.email)
     assert !newP.valid?,"Should not be valid as email is not unique"
     newP.email="zxczxc@zxczxczxc.com"
     assert newP.valid?
+  end
+
+protected
+  def create_person(options = {})
+    p = Person.new({ :email => 'micheal@example.com', :first_name => "Micheal", :last_name => "Tomson"}.merge(options))
+    p.save
+    p
   end
 
  end
