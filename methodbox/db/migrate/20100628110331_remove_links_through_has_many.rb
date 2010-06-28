4e17096a17a61e5f3d973696c40bd766f934cf5d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-06-28 15:13:14 +0000

    Remove all previous ways of linking things.  part of OBE-105

---
+++ methodbox/db/migrate/20100628110331_remove_links_through_has_many.rb
@@ -0,0 +1,13 @@
+class RemoveLinksThroughHasMany < ActiveRecord::Migration
+  def self.up
+    drop_table "script_lists"
+    drop_table "survey_lists"
+    drop_table "survey_to_script_lists"
+    drop_table "script_to_script_links"
+    drop_table "extract_to_extract_links"
+    drop_table "extract_to_extract_lists"
+  end
+
+  def self.down
+  end
+end
