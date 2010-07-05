8580a888b8b8df59a8fbd9b1dd28133154d5d413
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-07-05 11:14:38 +0000

    re-added some tables. remove them after next deployment

--- methodbox/db/migrate/20100628110331_remove_links_through_has_many.rb
+++ methodbox/db/migrate/20100628110331_remove_links_through_has_many.rb
@@ -1,10 +1,10 @@
 class RemoveLinksThroughHasMany < ActiveRecord::Migration
   def self.up
-    drop_table "script_lists"
-    drop_table "survey_lists"
-    drop_table "survey_to_script_lists"
-    drop_table "script_to_script_links"
-    drop_table "extract_to_extract_links"
+    # drop_table "script_lists"
+    # drop_table "survey_lists"
+    # drop_table "survey_to_script_lists"
+    # drop_table "script_to_script_links"
+    # drop_table "extract_to_extract_links"
     # drop_table "extract_to_extract_lists"
   end
 
