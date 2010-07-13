eebd0f93dd297e3e754ebf92d30dca7303de15cb
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-07-13 14:20:21 +0000

    exception notification plugin for emailing about errors which occur when deployed.  constant for localhost or not

--- methodbox/config/environment_local.rb
+++ methodbox/config/environment_local.rb
@@ -1,12 +1,14 @@
+LOCAL_REQUEST_ERROR = false
+ADMIN_CAN_SEE_DORMANT = true
 #used by the doi publication stuff
 ADMIN_EMAIL = "methodbox@gmail.com"
-SANDBOX = true
+SANDBOX = false
 EMAIL_ENABLED = true
 CSV_SERVER_LOCATION='localhost'
 CSV_SERVER_PORT='25000'
 CSV_SERVER_PATH='/csv-server/eos'
 CSV_FILE_PATH='/Users/Ian/obesity_data/csv/'
-REGISTRATION_CLOSED = false
+REGISTRATION_CLOSED = true
 NEW_CSV_FILE_PATH='/Users/Ian/obesity_data/csv/complete'
 METADATA_PATH='/Users/Ian/obesity_data/metadata/'
 UKDA_EMAIL_ADDRESS='http://oai.esds.ac.uk:8080/NesstarLogin/login.do'
