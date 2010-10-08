6d6d422eea7552561a845905942b369529f9710d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-10-08 12:53:40 +0000

    look at surveys/datasets using faceted browsing and the exhibit framework

--- methodbox/app/controllers/surveys_controller.rb
+++ methodbox/app/controllers/surveys_controller.rb
@@ -2,7 +2,7 @@ class SurveysController < ApplicationController
   
   before_filter :is_user_admin_auth, :only =>[ :new, :create]
 
-  before_filter :login_required, :except => [ :help, :help2, :index, :search_variables, :sort_variables, :show]
+  before_filter :login_required, :except => [ :help, :help2, :index, :search_variables, :sort_variables, :show, :exhibit]
 
   before_filter :find_previous_searches, :only => [ :index]
 
@@ -20,6 +20,24 @@ class SurveysController < ApplicationController
 
   before_filter :find_survey, :only => [:show, :edit, :update]
 
+  # browse surveys using exhibit
+  def exhibit
+    puts "exhibit json"
+    @surveys_json = "{types:{\"Dataset\":{pluralLabel:\"Datasets\"}},"
+     @surveys_json << "\"items\":["
+      Survey.all.each do |survey|
+        # @surveys_json << "{\"label\":\"" + survey.title + "\",\"year\":\"" + survey.year + "\",\"type\" : \"Survey\",\"survey_description\" :\"" + survey.description
+        #        @surveys_json << "\",\"survey_type\":\"" + survey.survey_type.shortname + "\"},"
+        survey.datasets.each do |dataset|
+          @surveys_json << "{\"label\":\"" + dataset.id.to_s + "\",\"name\":\"" + dataset.name + "\",\"survey-type\":\"" + survey.survey_type.shortname + "\",\"year\":\"" + survey.year + "\",\"type\" : \"Dataset\",\"dataset-description\" :\"" + dataset.description
+          @surveys_json << "\",\"Survey\":\"" + survey.title + "\",\"survey-description\" :\"" + survey.description + "\"},"
+        end
+      end
+      @surveys_json.chop!
+      @surveys_json << "]}"
+      puts @surveys_json.to_s
+  end
+  
   #only show 'my' links or 'all' links
   def show_links
     source_archives = []
