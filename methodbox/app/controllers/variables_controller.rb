f6d46ba7838c037bad62a37968e1ea690ebe1f65
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-10-22 15:40:52 +0000

    public access to surveys and scripts.  more sidebar actions.  spinner bar for category search.

--- methodbox/app/controllers/variables_controller.rb
+++ methodbox/app/controllers/variables_controller.rb
@@ -1,6 +1,6 @@
 class VariablesController < ApplicationController
 
-  before_filter :login_required, :except => [ :help, :open_pdf, :by_category, :show]
+  before_filter :login_required, :except => [ :help, :open_pdf, :by_category, :show, :find_for_multiple_surveys_by_category]
   before_filter :is_user_admin_auth, :only =>[ :deprecate_variable, :edit, :update, :create]
 
 
