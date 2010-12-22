d2cafa2439b72800759f30ff4d9d245a81aa502d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2010-12-22 13:35:51 +0000

    cart sorts correctly with actual number of variables rather than original, replace table header when removing from cart

--- methodbox/app/controllers/cart_controller.rb
+++ methodbox/app/controllers/cart_controller.rb
@@ -52,6 +52,7 @@ class CartController < ApplicationController
     end
 
     render :update, :status=>:created do |page|
+      page.replace_html "table_header", :partial => "surveys/table_header",:locals=>{:sorted_variables => @sorted_variables}
       page.replace_html "table_container", :partial=>"surveys/table",:locals=>{:sorted_variables=>@sorted_variables,:lineage => false, :extract_lineage => false, :extract_id => nil}
     end
     when "create_archive"
