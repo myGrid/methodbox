5b9ac22dd87bef64b88eda43b1c53d4da0ba7f62
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2011-08-16 16:31:18 +0100

    remove the chart creation from application js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -146,11 +146,6 @@ function downloadFromNesstar (nesstarUris) {
 	}
 }
 
-function drawChartForVariable(div_id, data) {
-        var chart = new google.visualization.BarChart($(div_id));
-        chart.draw(data, {width: 500, height: 200, is3D: true});
-}
-
 
 // document.observe('dom:loaded', function() {  
 //      $('new-group-link').observe('click', function(event) {  
