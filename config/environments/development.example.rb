4d70dfb95e447ce8d971fe5f5e1b61880fce1c33
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2012-02-06 13:32:11 +0000

    use jammit to load javascript and stylesheets remove the yui loader code - not needed since all js and css is now local move some inline css for the datatable expander to main_layout_extras

--- config/environments/development.example.rb
+++ config/environments/development.example.rb
@@ -35,5 +35,3 @@ ActionMailer::Base.smtp_settings = {
 #whether to use ssl or not. The routes.rb uses this
 ROUTES_PROTOCOL = "http"
 STATISTICS_ROUTE = '/12345/'
-#if you want to load a local copy of yui in development mode then add this constant, otherwise remove it
-YUI_LOADER="path/to/yui/build"
